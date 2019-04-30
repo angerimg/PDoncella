a79521ad23b3adc99081645bf989b563942f33c6
Angeri <amartinez@intellisys.com.do>
2019-04-30 09:36:12 -0400

    slideshow sin botones

--- app.js
+++ app.js
@@ -2,39 +2,10 @@
 $(document).ready(function () {
 
     //Empieza con las funcionalidades del slide show
-    //boton anterior
-    $('#previous').on('click', function () {
-        //console.log('::Running');
-
-        // Change to the previous image
-        $('#im_' + currentImage).fadeOut("fast", function () {
-            decreaseImage();
-            $('#im_' + currentImage).fadeIn("fast");
-        });
-
-
-    });
-    //boton siguiente 
-    $('#next').on('click', function () {
-        // Change to the next image
-        $('#im_' + currentImage).stop().fadeOut(1);
-        increaseImage();
-        $('#im_' + currentImage).stop().fadeIn(1);
-    });
-
     //codigo para calcular la imagen
     var currentImage = 1;
     var totalImages = 3;
 
-    function increaseImage() {
-        /* Increase currentImage by 1.
-        * Resets to 1 if larger than totalImages
-        */
-        currentImage++;
-        if (currentImage == totalImages + 1) {
-            currentImage = 1;
-        }
-    }
     function decreaseImage() {
         /* Decrease currentImage by 1.
         * Resets to totalImages if smaller than 1
@@ -48,6 +19,10 @@ $(document).ready(function () {
 
     //codigo para automatizar el slide
     window.setInterval(function () {
-        $('#previous').click();
+        //$('#previous').click();
+        $('#im_' + currentImage).fadeOut("fast", function () {
+            decreaseImage();
+            $('#im_' + currentImage).fadeIn("fast");
+        });
     }, 2500);
 });
